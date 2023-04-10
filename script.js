47a88e4296dd8dbbbbe56f29474d7d8d15138105
poggi86 <129793273+poggi86@users.noreply.github.com>
2023-04-10 20:34:21 +0200

    final commit

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-//Menü, welches sich bei Handys einlkappt 
-
+//Menü, das sich bei Handys ein- und ausklappen lässt
 const hamburger = document.querySelector('button[aria-expanded]');
 
-function klappNav({
-  target
-}) {
+function klappNav({ target }) {
   const expanded = target.getAttribute('aria-expanded') === 'true' || false;
   hamburger.setAttribute('aria-expanded', !expanded);
 }
 
 hamburger.addEventListener('click', klappNav);
 
-// Funktion, die den Bereich unter dem Dropdownmenue zur Abholung steuert
+//Funktion, die den Bereich unter dem Dropdownmenü zur Abholung steuert
 function setVisibility(select) {
   var val = select.value;
   document.getElementById('id_geschaeftsstelle').style.display = (val === 'stelle' ? "block" : "none");
   document.getElementById('id_abholung').style.display = (val !== 'stelle' ? "block" : "none");
 }
 
-// Definitionen für die Formulare 
+//Definitionen für die Formulare 
 let krisen_region = document.querySelector('#krisen_region')
 let street = document.querySelector('#street')
 let postCode = document.querySelector('#post_code');
@@ -26,7 +23,7 @@ let timeStamp = document.querySelector('#time_stamp');
 let city = document.querySelector('#city');
 let chBx = document.querySelectorAll("input[name='art_kleidung']");
 
-// Formular für die Abholung 
+//Formular für die Abholung 
 document.forms[0].onsubmit = event => {
   let validPostCode = postCode.value.startsWith('78');
   if (!validPostCode) {
@@ -34,8 +31,8 @@ document.forms[0].onsubmit = event => {
     window.alert('Du wohnst leider außerhalb unseres Einzugbereiches. Bitte komm mit deiner Spende zur Geschäftsstelle.');
   }
 
-  // Zusammenfassung für Abholung
-  // Herren
+  //Zusammenfassung für Abholung
+  //Herren
   if (validPostCode && chBx[0].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[0].value}
@@ -46,10 +43,10 @@ document.forms[0].onsubmit = event => {
     Vielen Dank für Deine Hilfe.
     Bitte halte die Kleidung bereits eingepackt bereit.
     Wir kommen zwischen 09:00 und 16:00 Uhr.
-    Dein Team der Kleiderspende
-    `);
+    Dein Team der Kleiderspende`);
   }
-  // Damen
+
+  //Damen
   if (validPostCode && chBx[1].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[1].value}
@@ -60,10 +57,10 @@ document.forms[0].onsubmit = event => {
     Vielen Dank für Deine Hilfe.
     Bitte halte die Kleidung bereits eingepackt bereit.
     Wir kommen zwischen 09:00 und 16:00 Uhr.
-    Dein Team der Kleiderspende
-    `);
+    Dein Team der Kleiderspende`);
   }
-  // Kinder
+
+  //Kinder
   if (validPostCode && chBx[2].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[2].value}
@@ -74,10 +71,10 @@ document.forms[0].onsubmit = event => {
     Vielen Dank für Deine Hilfe.
     Bitte halte die Kleidung bereits eingepackt bereit.
     Wir kommen zwischen 09:00 und 16:00 Uhr.
-    Dein Team der Kleiderspende
-    `);
+    Dein Team der Kleiderspende`);
   }
-  // Baby
+
+  //Baby
   if (validPostCode && chBx[3].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[3].value}
@@ -88,11 +85,11 @@ document.forms[0].onsubmit = event => {
     Vielen Dank für Deine Hilfe.
     Bitte halte die Kleidung bereits eingepackt bereit.
     Wir kommen zwischen 09:00 und 16:00 Uhr.
-    Dein Team der Kleiderspende
-    `);
+    Dein Team der Kleiderspende`);
   }
 }
-// Zusammenfassung für die Abgabe in der Geschäftsstelle
+
+//Zusammenfassung für die Abgabe in der Geschäftsstelle
 document.forms[1].onsubmit = event => {
   let validtime_stamp = timeStamp.value.startsWith('2');
   if (!validtime_stamp) {
@@ -100,48 +97,57 @@ document.forms[1].onsubmit = event => {
     window.alert('Du hast leider ein falsches Zeitformat eingegeben. Nutze den Kalender zur Eingabe.');
   }
 
-  // Herren
+  //Herren
   if (validtime_stamp && chBx[0].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[0].value}
     Region: ${krisen_region.value}
     Abgabedatum: ${timeStamp.value}
     Vielen Dank für Deine Hilfe.
-    Bitte lege die Kleidung in die bereitgestellte Box.
-    Dein Team der Kleiderspende
-    `);
+    Lege die Kleidung während der Geschäftszeiten in die Box.
+    Geschäftszeiten: Montag bis Donnerstag 11.00 - 17.00 Uhr.
+    Dein Team der Kleiderspende`);
   }
-  // Damen Geschäftsstelle
+
+  //Damen Geschäftsstelle
   if (validtime_stamp && chBx[1].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[1].value}
     Region: ${krisen_region.value}
     Abgabedatum: ${timeStamp.value}
     Vielen Dank für Deine Hilfe.
-    Bitte lege die Kleidung in die bereitgestellte Box.
-    Dein Team der Kleiderspende
-    `);
+    Lege die Kleidung während der Geschäftszeiten in die Box.
+    Geschäftszeiten: Montag bis Donnerstag 11.00 - 17.00 Uhr.
+    Dein Team der Kleiderspende`);
   }
-  // Kinder Geschäftsstelle
+
+  //Kinder Geschäftsstelle
   if (validtime_stamp && chBx[2].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[2].value}
     Region: ${krisen_region.value}
     Abgabedatum: ${timeStamp.value}
     Vielen Dank für Deine Hilfe.
-    Bitte lege die Kleidung in die bereitgestellte Box.
-    Dein Team der Kleiderspende
-    `);
+    Lege die Kleidung während der Geschäftszeiten in die Box.
+    Geschäftszeiten: Montag bis Donnerstag 11.00 - 17.00 Uhr.
+    Dein Team der Kleiderspende`);
   }
-  // Baby Geschäftsstelle
+
+  //Baby Geschäftsstelle
   if (validtime_stamp && chBx[3].checked) {
     window.alert(`Zusammenfassung:
     Kleiderwahl: ${chBx[3].value}
     Region: ${krisen_region.value}
     Abgabedatum: ${timeStamp.value}
     Vielen Dank für Deine Hilfe.
-    Bitte lege die Kleidung in die bereitgestellte Box.
-    Dein Team der Kleiderspende
-    `);
+    Lege die Kleidung während der Geschäftszeiten in die Box.
+    Geschäftszeiten: Montag bis Donnerstag 11.00 - 17.00 Uhr.
+    Dein Team der Kleiderspende`);
   }
+}
+
+//Ausschluss von vergangenen Tagen im Kalender
+window.onload = function () {
+  var date = new Date().toISOString().split('T')[0];
+  document.getElementsByName("kalender_abholung")[0].setAttribute('min', date);
 }
